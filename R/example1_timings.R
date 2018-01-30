f1dfaf2fbf2069f2b702bac160daf1957527c8a5
Colin Gillespie <csgillespie@gmail.com>
2018-01-30 11:54:30 +0000

    Add timing data frames

--- R/example1_timings.R
+++ R/example1_timings.R
@@ -13,15 +13,16 @@ mis_sigma = matrix(c(0.012, 0.0052, 0.0052, 0.0046),
                    ncol = 2, byrow = TRUE)
 
 # Run sampler & time
-mis_timings = numeric(reps)
+dd_mis = data.frame(elapsed = 0, ess = numeric(reps), ess_sec = 0)
 for(i in 1:reps) {
   tim = system.time({id_mis <- mis(sde = id_sde, obs = id_ods, 
                                    sigma = mis_sigma, pars_init = pars_init,
                                    log_prior = id_log_prior, deltat = 0.2, inter = 1, 
                                    iters = 20000)})
-  mis_timings[i] = min(effectiveSize(id_mis))/tim["elapsed"]
+  dd_mis[i,] = c(tim["elapsed"], min(effectiveSize(id_mis)), min(effectiveSize(id_mis))/tim["elapsed"])
 }
-mis_timings
+signif(colMeans(dd_mis),3)
+saveRDS(dd_mis, file = "data/mis_timings.rds")
 #16.11 17.73
 
 
@@ -35,11 +36,11 @@ cor_sigma = matrix(c(0.023, 0.0061, 0.0061, 0.0053),
                    ncol = 2, byrow = TRUE)
 
 #Run sampler and plot output appropriately
-rhos = c(0, 0.5, 0.9, 0.99)
-dd = data.frame(rho = 0, elapsed = 0, ess = numeric(length(rhos)), N=0)
+rhos = c(0, 0.5, 0.9, 0.95,0.99)
+dd_cor = data.frame(rho = 0, elapsed = 0, ess = numeric(length(rhos)), N=0)
 i = 1
 k = 1
-for(r in 1:2) {
+for(r in 1:reps) {
   for(i in seq_along(rhos)) {
     for(n in c(1, 2, 10, 100)) {
       t = system.time({id_cor <- correlated(sde = id_sde, obs = id_ods,
@@ -47,21 +48,35 @@ for(r in 1:2) {
                                            log_prior = id_log_prior, deltat = 0.2, inter = 1, 
                                            iters = 20000, N = n, rho = rhos[i])})
                       
-                      dd[k,] = c(rhos[i], t[3], ess = min(effectiveSize(id_cor)), N=n)
+                      dd_cor[k,] = c(rhos[i], t[3], ess = min(effectiveSize(id_cor)), N=n)
                       k = k + 1
     }
   }
 }
-#saveRDS(dd, file= "res.rds")
+saveRDS(dd_cor, file= "data/cor_timings.rds")
 #dd = readRDS("res.rds")
 
 library("dplyr")
 library("tidyr")
-head(dd)
 
-sum = dd %>%
+
+sum = dd_cor %>%
   group_by(N, rho) %>%
-  summarise(ess_sec = mean(ess)/mean(elapsed), ess = mean(ess)) %>%
-  gather(type, value, -N, -rho)
-sum
+  summarise(time = mean(elapsed), ess = mean(ess), ess_sec = mean(ess)/mean(elapsed))
 
+## PMMH
+dd_pmmh = data.frame(elapsed = 0, ess = numeric(reps), ess_sec = 0)
+for(i in 1:reps) {
+  tim = system.time({id_pmmh <- correlated(sde = id_sde, obs = id_ods,
+                                           sigma = cor_sigma, pars_init = pars_init,
+                                           log_prior = id_log_prior, deltat = 0.2, inter = 1, 
+                                           iters = 20000, N = 50, rho = 0)})
+  dd_mis[i,] = c(tim["elapsed"], min(effectiveSize(id_pmmh)), min(effectiveSize(id_pmmh))/tim["elapsed"])
+}
+#saveRDS(dd_mis, file = "data/pmmh_timings.rds")
+
+signif(colMeans(dd_mis), 3)
+
+
+RPushbullet::pbPost("note", title = "Completed")
+  
